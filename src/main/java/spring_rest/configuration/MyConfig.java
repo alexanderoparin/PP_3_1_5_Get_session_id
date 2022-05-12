d88ad956a255156858c3c31f16373fff8936c4d5
Alexander Oparin <alexanderoparin@mail.ru>
2022-05-12 12:14:24 +0300

    Initial commit

---
+++ src/main/java/spring_rest/configuration/MyConfig.java
@@ -0,0 +1,9 @@
+package spring_rest.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("spring_rest")
+public class MyConfig {
+}
